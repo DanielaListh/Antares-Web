fbc44a7dbed2090e8ceb8f86db33e005d704be3a
DanielaListh <dani_moe_berrios@hotmail.com>
2024-10-20 06:51:43 -0300

    creacion de modulos y rutas para tabla de roles

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const app = express();
 
 app.use(express.json()); // en el cuerpo de la peticion vendra un json y se transforma en un objeso js y asi poder usarlo
 
+// ruta para tablas especialidades medicas
 const especialidadesMedicasRouter = require('./routers/especialidades_medicas.router'); // que coloque en esta constante lo que hay en ese modulo
 app.use('/especialidades', especialidadesMedicasRouter); // prefijo de la ruta especialidades
 
+//ruta pata tabla de roles
+const rolesRouter = require('./routers/roles.router'); // que coloque en esta constante lo que hay en ese modulo
+app.use('/roles', rolesRouter); // prefijo de la ruta roles
 
 
 app.get('/', (req,res) => { // la ruta raiz del proyecto o pag principal del sitio
